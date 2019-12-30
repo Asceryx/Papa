69dde738ee8e0265868fda28ee174ef19d3fa908
Asceryx <jhayrault@gmail.com>
2019-12-30 17:09:35 +0100

    interface

--- test/LED.py
+++ test/LED.py
@@ -4,7 +4,7 @@ import time
 
 if __name__ == '__main__':
     b = Board(GPIO.BOARD)
-    c1 = Captor({12: GPIO.IN, 14: GPIO.OUT})
+    c1 = Captor({12: GPIO.IN})
     while True:
         c1.write(GPIO.HIGH)
         time.sleep(3)
