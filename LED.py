4f8c72d1f41f49d7abde95a07317925e2ab1630a
Asceryx <jhayrault@gmail.com>
2019-12-30 17:34:44 +0100

    import

--- LED.py
+++ LED.py
@@ -7,6 +7,6 @@ if __name__ == '__main__':
     b = Board(GPIO.BOARD)
     c1 = Captor({12: GPIO.IN})
     while True:
-        c1.write(GPIO.HIGH)
+        c1.write(12, GPIO.HIGH)
         time.sleep(3)
-        c1.write(GPIO.LOW)
+        c1.write(12, GPIO.LOW)
