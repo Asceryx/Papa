bfdd43e2339f2c467a25ac8a205b3f482c8846d8
Asceryx <jhayrault@gmail.com>
2019-12-31 16:59:14 +0100

    multiLED - Worker

--- system/Worker.py
+++ system/Worker.py
@@ -8,9 +8,8 @@ class Worker(Thread):
     def __init__(self, fct, *args):
         Thread.__init__(self)
         self.args = args
-        self.fct = [f for f in fct]
+        self.fct = fct
 
     def run(self):
         with lock:
-            for f in self.fct:
-                f(self.args)
+            self.f(self.args)
