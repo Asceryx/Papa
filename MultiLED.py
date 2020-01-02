644ccd6db88f47e9c22a64db460c14fe05f44ad3
Asceryx <jhayrault@gmail.com>
2020-01-02 15:41:28 +0100

    thread

--- MultiLED.py
+++ MultiLED.py
@@ -41,12 +41,12 @@ if __name__ == '__main__':
     c6 = Captor({36: GPIO.OUT})
 
     # Worker : do flashing
-    w1 = Worker(flash, 10, c1, 12)
-    w2 = Worker(flash, 5, c2, 16)
-    w3 = Worker(flash, 3, c3, 18)
-    w4 = Worker(flash, 30, c4, 22)
-    w5 = Worker(flash, 12, c5, 32)
-    w6 = Worker(flash, 15, c6, 36)
+    w1 = Worker(target=flash, args=(10, c1, 12,))
+    w2 = Worker(target=flash, args=(5, c2, 16,))
+    w3 = Worker(target=flash, args=(3, c3, 18))
+    w4 = Worker(target=flash, args=(30, c4, 22))
+    w5 = Worker(target=flash, args=(12, c5, 32))
+    w6 = Worker(target=flash, args=(15, c6, 36))
 
     # Worker : Launch work
     w1.start()
