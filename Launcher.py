57651912c22892214be8ac73f7be877c6818d2ef
Asceryx <jhayrault@gmail.com>
2020-09-06 20:43:37 +0200

    correction LedImpl

--- Launcher.py
+++ Launcher.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     pin18 = Output(channel=18)
     led = LedImpl("led", pin18, "led5v", "Led de test")
     led.light(True)
-    time.sleep(1)
+    time.sleep(10)
     led.light(False)
     blink_service = BlinkService(led)
     blink_service.run(1, 1, True)
