c2912182e66d0f2a99f7b2d329c9a65919d9116b
Asceryx <jhayrault@gmail.com>
2020-09-06 10:29:05 +0200

    LedImpl

--- implementation/LedImpl.py
+++ implementation/LedImpl.py
@@ -6,9 +6,10 @@ from models.Led import Led
 
 class LedImpl(Led):
     def __init__(self, name, pin: Output, reference, description):
-        super().__init__(name, pin, reference, description)
         self._gpio = GPIO(self.pin.channel, self.pin.type)
         self._pmw = GPIO.PWM(self.pin.channel, 100)
+        super().__init__(name, pin, reference, description)
+
 
     @property
     def shutdown(self):
