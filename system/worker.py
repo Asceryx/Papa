04d37a6822f5f79017ba137ed49adef11a180070
Asceryx <jhayrault@gmail.com>
2020-09-06 17:59:59 +0200

    correction LedImpl

--- system/worker.py
+++ system/worker.py
@@ -9,7 +9,7 @@ class Worker(Thread):
     def __init__(self, *args, **kwargs):
         Thread.__init__(self, *args, **kwargs)
         self._return = None
-        self.__thr_exit = None
+        self.__thr_exit = False
 
     def run(self):
         target = getattr(self, '_target')
