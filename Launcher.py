d52a2d66a9a1e122a8194b1b5568770fcbe36476
Asceryx <jhayrault@gmail.com>
2020-09-06 19:11:34 +0200

    correction LedImpl

--- Launcher.py
+++ Launcher.py
@@ -6,8 +6,8 @@ from models.Led import Led
 from services.blinkService import BlinkService
 
 if __name__ == '__main__':
-    pin17 = Output(channel=17)
-    led = LedImpl("led", pin17, "led5v", "Led de test")
+    pin35 = Output(channel=35)
+    led = LedImpl("led", pin35, "led5v", "Led de test")
     led.light(True)
     time.sleep(1)
     led.light(False)
